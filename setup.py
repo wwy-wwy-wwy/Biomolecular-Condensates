94f6166b682d9635a82bdfe63a5022fd18bcee74
Wenyun Wang <77664445+wwy-wwy-wwy@users.noreply.github.com>
2022-04-19 17:59:44 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='condensate_speckles',
+      version='1.0',
+      description='A package that studies the decay behaviour of FUS protein',
+      url='https://github.com/phys201/Condensate_speckles',
+      author='Anqi Chen, Wenyun Wang, Hanyue Wang',
+      author_email='',
+      license='GPLv3',
+      packages=['condensate_speckle'],
+      install_requires=['pymc3'])
