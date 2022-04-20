31dfbc288a0c29956f8225aa1e6d4fda5a5493be
Wenyun Wang <wwang@g.harvard.edu>
2022-04-19 20:29:58 -0400

    test

--- condensate_speckle/tests/test_io.py
+++ condensate_speckle/tests/test_io.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
-from example.data_io import get_example_data_file_path, load_data
+from condensate_speckle.data_io import get_example_data_file_path, load_data
 import pandas as  pd
 
 class TestIo(TestCase):
     def test_data_io(self):
         data = load_data(get_example_data_file_path('simulated_data.txt'))
-        assert data.x[0] == 1
+        assert data.0[0]>73
