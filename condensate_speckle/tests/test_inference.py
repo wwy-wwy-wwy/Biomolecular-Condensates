d56295187cc447693b85201acc4de1920ee149bf
Wenyun Wang <wwang@g.harvard.edu>
2022-04-19 21:41:58 -0400

    nan

--- condensate_speckle/tests/test_inference.py
+++ condensate_speckle/tests/test_inference.py
@@ -11,7 +11,7 @@ class TestModel(TestCase):
         data = load_data('simulated_data.csv',data_dir='../example_data').to_numpy()
         ar1_model=set_model(data)
         estimate = pm.find_MAP(model = ar1_model)
-        assert np.allclose(estimate['decay_time'], 10.24767332, rtol=0, atol=1)
+        assert np.allclose(estimate['decay_time'], 10.24767332, rtol=0, atol=1.1)
 
 if __name__ == '__main__':
     unittest.main()
