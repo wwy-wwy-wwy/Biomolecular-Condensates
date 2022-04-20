7a95efb01580e4c5a3d672f4b0d693d8e291a55e
Hanyue-W <78049119+Hanyue-W@users.noreply.github.com>
2022-04-19 22:32:04 -0400

    update metadata

--- condensate_speckle/tests/test_io.py
+++ condensate_speckle/tests/test_io.py
@@ -13,4 +13,10 @@ class TestIo(TestCase):
         mean, variance = analyze_data('simulated_data.csv',data_dir='../example_data')
         assert np.allclose(mean, 83.3, rtol=0, atol=1)
         
+    def test_data_mean2(self):
+        mean, variance = analyze_data('test.csv',data_dir='../example_data')
+        meta_mean = load_data('test.csv',data_dir='../example_data').to_numpy()[3,1]
+        assert np.allclose(mean, meta_mean, rtol=0, atol=1)
+        
+        
     
