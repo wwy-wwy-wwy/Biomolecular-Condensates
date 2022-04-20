ebfc800383b1b725f9462d18accd31407945ccf0
Wenyun Wang <wwang@g.harvard.edu>
2022-04-19 21:21:52 -0400

    /////

--- condensate_speckle/tests/.ipynb_checkpoints/test_io-checkpoint.py
+++ condensate_speckle/tests/.ipynb_checkpoints/test_io-checkpoint.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 from condensate_speckle.data_io import get_example_data_file_path, load_data
 import pandas as  pd
+import numpy as np
 
 class TestIo(TestCase):
     def test_data_io(self):
         
-        data = load_data('simulated_data.csv',data_dir='../example_data')
+        data = load_data('simulated_data.csv',data_dir='../example_data').to_numpy()
         assert data[0,0]==0
