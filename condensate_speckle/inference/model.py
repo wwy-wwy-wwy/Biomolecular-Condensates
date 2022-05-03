0cf804f7fd2f25d6d31453395e40bd40ab5de020
Anqi Chen <anqichen@g.harvard.edu>
2022-05-03 15:20:59 -0400

    add plot for the inference result of the camera noise

--- condensate_speckle/inference/model.py
+++ condensate_speckle/inference/model.py
@@ -138,7 +138,7 @@ def plot_posterior(trace, n_time_scale = 1, var_names = ['decay_time','precision
     '''
     
     if n_time_scale == 2:
-        var_names = ['decay_time_1', 'decay_time_2', 'precision_1', 'precision_2']
+        var_names = ['decay_time_1', 'decay_time_2', 'precision_1', 'precision_2','noise_std']
     
     az.plot_posterior(
     trace,
