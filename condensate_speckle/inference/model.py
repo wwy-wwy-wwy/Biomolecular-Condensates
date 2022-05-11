d0edcd752b9205f32bd3c831ea630be9e793f0d5
Wenyun Wang <wwang@g.harvard.edu>
2022-05-11 17:02:33 -0400

    update camera noise std using truncated normal

--- condensate_speckle/inference/model.py
+++ condensate_speckle/inference/model.py
@@ -32,9 +32,8 @@ def set_model(data,quantization):
         # weak correlation with the other parameters anyway
         observed_mean = np.mean(data)   
         #camera_noise_std=8.787413879857576
-        camera_noise_std_center = observed_mean/12
-        #camera_noise_std = pm.Uniform("noise_std", lower=0, upper=quantization)
-        camera_noise_std = pm.Normal("noise_std", mu=camera_noise_std_center, sigma=5)
+        camera_noise_std_mean = np.sqrt(observed_mean)*0.71
+        camera_noise_std = pm.TruncatedNormal("noise_std", mu=camera_noise_std_mean, sigma=5,lower=0)
     
         true = pm.AR1("y", k=stationarity, tau_e=precision_AR1, shape=len(data))
         likelihood = pm.Normal("likelihood", mu=(true + observed_mean), sigma=camera_noise_std, observed=data)
@@ -79,9 +78,8 @@ def set_double_scale_model(data, quantization):
         
         #camera_noise_std=8.787413879857576
         #camera_noise_std = pm.Uniform("noise_std", lower=0, upper=quantization)
-        camera_noise_std_center = observed_mean/12
-        #camera_noise_std = pm.Uniform("noise_std", lower=0, upper=quantization)
-        camera_noise_std = pm.Normal("noise_std", mu=camera_noise_std_center, sigma=5)
+        camera_noise_std_mean = np.sqrt(observed_mean)*0.71
+        camera_noise_std = pm.TruncatedNormal("noise_std", mu=camera_noise_std_mean, sigma=5,lower=0)
     
         true1 = pm.AR1("y1", k=stationarity1, tau_e=precision_1, shape=len(data))
         true2 = pm.AR1("y2", k=stationarity2, tau_e=precision_2, shape=len(data))
@@ -124,9 +122,8 @@ def set_single_precision_model(data,quantization):
         # weak correlation with the other parameters anyway
         observed_mean = np.mean(data)   
         #camera_noise_std is a normal distribution centered at somewh
-        camera_noise_std_center = observed_mean/12
-        #camera_noise_std = pm.Uniform("noise_std", lower=0, upper=quantization)
-        camera_noise_std = pm.Normal("noise_std", mu=camera_noise_std_center, sigma=5)
+        camera_noise_std_mean = np.sqrt(observed_mean)*0.71
+        camera_noise_std = pm.TruncatedNormal("noise_std", mu=camera_noise_std_mean, sigma=5,lower=0)
     
         true1 = pm.AR1("y_1", stationarity, tau_e=precision_AR1, shape=len(data))
         true2 = pm.AR1("y_2", stationarity2, tau_e=precision_AR1, shape=len(data))  
