c2f9964bb2b4ff1dc79e8019d4c1bdd4daef45c7
Wenyun Wang <wwang@g.harvard.edu>
2022-05-12 19:10:56 -0400

    update model

--- condensate_speckle/inference/model.py
+++ condensate_speckle/inference/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pymc3 as pm
 import arviz as az
 
+<<<<<<< Updated upstream
 def set_2h_model(data,quantization):
     
     '''
@@ -41,6 +42,47 @@ def set_2h_model(data,quantization):
     return ar1_model
 
 def set_model(data,quantization):
+=======
+# def set_2h_model(data,quantization):
+    
+#     '''
+#     This function read in the experiment data, and generate an AR1 model based on it.
+    
+#     input: 
+#     -----------------
+#     data: ndarray, 
+#     quantization: float
+    
+#     return: 
+#     -----------------
+#     AR1 model
+    
+#     '''
+
+#     # Bayesian parameter estimation with pymc3
+#     ar1_model = pm.Model()
+
+#     with ar1_model:
+#         # 'phi'is ln(-1/tau) used in our generative model
+#         decay_time = pm.Exponential("decay_time", lam=1) 
+#         stationarity = pm.Deterministic("stationarity", np.exp(-1/decay_time))
+
+#         # 'precision' is 1/(variance of innovation). As we use normalized data, this term has to be divided by intensity_mean squared
+#         precision_AR1 = pm.Uniform("precision", lower = 0 , upper = 10) 
+
+#         # process mean: use observed mean since process is assumed to be stationary, and there should be
+#         # weak correlation with the other parameters anyway
+#         observed_mean = np.mean(data)
+#         camera_noise_std_mean = np.sqrt(observed_mean)
+    
+#         true = pm.AR1("y", k=stationarity, tau_e=precision_AR1, shape=len(data))
+#         likelihood = pm.Normal("likelihood", mu=(true + observed_mean), sigma=camera_noise_std_mean, observed=data)
+        
+
+#     return ar1_model
+
+def set_model(data,quantization,aged_time):
+>>>>>>> Stashed changes
     
     '''
     This function read in the experiment data, and generate an AR1 model based on it.
@@ -60,8 +102,15 @@ def set_model(data,quantization):
     ar1_model = pm.Model()
 
     with ar1_model:
+<<<<<<< Updated upstream
         # 'phi'is ln(-1/tau) used in our generative model
         decay_time = pm.Uniform("decay_time", lower=0,upper=1.5*len(data)) 
+=======
+        if aged_time=='2h':
+            decay_time = pm.Exponential("decay_time", lam=1) 
+        else:
+            decay_time = pm.Uniform("decay_time", lower=0,upper=1.5*len(data)) 
+>>>>>>> Stashed changes
         stationarity = pm.Deterministic("stationarity", np.exp(-1/decay_time))
 
         # 'precision' is 1/(variance of innovation). As we use normalized data, this term has to be divided by intensity_mean squared
@@ -79,52 +128,61 @@ def set_model(data,quantization):
     return ar1_model
                
 
-def set_double_scale_model(data, quantization):
-    '''
-    This function read in the experiment data, and generate a two-timescale AR1 model based on it.
+# def set_double_scale_model(data, quantization):
+#     '''
+#     This function read in the experiment data, and generate a two-timescale AR1 model based on it.
     
-    input: 
-    -----------------
-    data: ndarray, 
-    quantization: float
+#     input: 
+#     -----------------
+#     data: ndarray, 
+#     quantization: float
     
-    return: 
-    -----------------
-    two-timescale AR1 model
+#     return: 
+#     -----------------
+#     two-timescale AR1 model
     
-    '''
+#     '''
     
-    ar1_two_timescales_model = pm.Model()
+#     ar1_two_timescales_model = pm.Model()
 
-    with ar1_two_timescales_model:
-        # 'phi'is ln(-1/tau) used in our generative model
-        decay_time1 = pm.Uniform("decay_time_1",lower = 0, upper = 500) 
-        decay_time_split = pm.Uniform("decay_time_split",lower = 0, upper = 500)
-        decay_time2 = pm.Deterministic("decay_time_2",decay_time1 + decay_time_split)
+#     with ar1_two_timescales_model:
+#         # 'phi'is ln(-1/tau) used in our generative model
+#         decay_time1 = pm.Uniform("decay_time_1",lower = 0, upper = 500) 
+#         decay_time_split = pm.Uniform("decay_time_split",lower = 0, upper = 500)
+#         decay_time2 = pm.Deterministic("decay_time_2",decay_time1 + decay_time_split)
     
-        stationarity1 = pm.Deterministic("stationarity_1", np.exp(-1/decay_time1))
-        stationarity2 = pm.Deterministic("stationarity_2", np.exp(-1/decay_time2))
+#         stationarity1 = pm.Deterministic("stationarity_1", np.exp(-1/decay_time1))
+#         stationarity2 = pm.Deterministic("stationarity_2", np.exp(-1/decay_time2))
 
-        # 'precision' is 1/(variance of innovation). As we use normalized data, this term has to be divided by intensity_mean squared
-        precision_1 = pm.Uniform("precision_1", lower = 0 , upper = 10) 
-        precision_2 = pm.Uniform("precision_2", lower = 0 , upper = 10) 
+#         # 'precision' is 1/(variance of innovation). As we use normalized data, this term has to be divided by intensity_mean squared
+#         precision_1 = pm.Uniform("precision_1", lower = 0 , upper = 10) 
+#         precision_2 = pm.Uniform("precision_2", lower = 0 , upper = 10) 
     
-        # process mean: use observed mean since process is assumed to be stationary, and there should be
-        # weak correlation with the other parameters anyway
-        observed_mean = np.mean(data)
+#         # process mean: use observed mean since process is assumed to be stationary, and there should be
+#         # weak correlation with the other parameters anyway
+#         observed_mean = np.mean(data)
         
+<<<<<<< Updated upstream
         observed_mean = np.mean(data)
         camera_noise_std_mean = np.sqrt(data)
     
         true1 = pm.AR1("y1", k=stationarity1, tau_e=precision_1, shape=len(data))
         true2 = pm.AR1("y2", k=stationarity2, tau_e=precision_2, shape=len(data))
         likelihood = pm.Normal("likelihood", mu=(true1+ true2 + observed_mean), sigma=camera_noise_std_mean, observed=data)
+=======
+#         observed_mean = np.mean(data)
+#         camera_noise_std_mean = np.sqrt(data)
+    
+#         true1 = pm.AR1("y1", k=stationarity1, tau_e=precision_1, shape=len(data))
+#         true2 = pm.AR1("y2", k=stationarity2, tau_e=precision_2, shape=len(data))
+#         likelihood = pm.Normal("likelihood", mu=(true1+ true2 + observed_mean), sigma=camera_noise_std_mean, observed=data)
+>>>>>>> Stashed changes
         
-    return ar1_two_timescales_model
+#     return ar1_two_timescales_model
 
 
 
-def set_single_precision_model(data,quantization):
+def set_single_precision_model(data,quantization, aged_time):
     
     '''
     This function read in the experiment data, and generate an AR1 model based on it.
@@ -145,6 +203,7 @@ def set_single_precision_model(data,quantization):
 
     with ar1_new_model:
         # 'phi'is ln(-1/tau) used in our generative model
+        
         decay_time = pm.Uniform("decay_time_1",lower = 0, upper = 500) 
         stationarity = pm.Deterministic("stationarity_1", np.exp(-1/decay_time))
         decay_time_split = pm.Uniform("decay_time_split",lower = 0, upper = 500)
@@ -228,7 +287,7 @@ def plot_posterior(trace, n_time_scale = 1, var_names = ['decay_time','precision
 );
   
     
-def plot_pair(trace, n_time_scale = 1, var_names = ['decay_time','precision','noise_std']):
+def plot_pair(trace, n_time_scale = 1, var_names = ['decay_time','precision']):
     '''
     This function plots the posterior of sampling from the single or the multiple time scale model
     
@@ -240,7 +299,7 @@ def plot_pair(trace, n_time_scale = 1, var_names = ['decay_time','precision','no
     '''
     
     if n_time_scale == 2:
-        var_names = ['decay_time_1', 'decay_time_2', 'precision','noise_std']
+        var_names = ['decay_time_1', 'decay_time_2', 'precision']
     
     az.plot_pair(
     trace,
